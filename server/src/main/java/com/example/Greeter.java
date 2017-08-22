0de6febd6ea9ae6dd5d6a5bcf2027519d365d9be
Erwin Bothof <erwinb@topdesk.com>
2017-08-22 13:55:19 +0200

    bugfix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
 /**
-*@param bla bla
-*@return bla bla bla
+* @param bla bla
+* @return bla bla bla
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
